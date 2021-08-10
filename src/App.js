f20ac802ac01fdbdb5e1bcf3b075cbb93110e764
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-08-10 11:38:36 +0800

    new expense form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import ExpenseItem from "./component/ExpenseItem";
+import ExpenseItem from "./component/Expenses/ExpenseItem";
+import Expenses from "./component/Expenses/Expenses";
+import NewExpense from "./component/NewExpense/NewExpense";
 function App() {
 	const expenses = [
 		{
@@ -28,27 +30,8 @@ function App() {
 	];
 	return (
 		<div>
-			<h2>Let's get started!</h2>
-			<ExpenseItem
-				title={expenses[0].title}
-				amount={expenses[0].amount}
-				date={expenses[0].date}
-			></ExpenseItem>
-			<ExpenseItem
-				title={expenses[1].title}
-				amount={expenses[1].amount}
-				date={expenses[1].date}
-			></ExpenseItem>
-			<ExpenseItem
-				title={expenses[2].title}
-				amount={expenses[2].amount}
-				date={expenses[2].date}
-			></ExpenseItem>
-			<ExpenseItem
-				title={expenses[3].title}
-				amount={expenses[3].amount}
-				date={expenses[3].date}
-			></ExpenseItem>
+			<NewExpense />
+			<Expenses expenses={expenses} />
 		</div>
 	);
 }
